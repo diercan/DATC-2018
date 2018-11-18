69e2e48a41c7f6447f64425c735dcfd206ecb59a
IndreicaNicoleta <indreica_nicoleta@yahoo.com>
2018-11-18 19:49:37 +0200

    Background Worker - Verified if project can connect to a Google API Updated Architecture slide in powerpoint - Using Cloud Sql Admin API that has direct access to the database.

--- Indreica_Claudia-Nicoleta/Proiect_Datc/BackGroundWorker/BackGroundWorker/Program.cs
+++ Indreica_Claudia-Nicoleta/Proiect_Datc/BackGroundWorker/BackGroundWorker/Program.cs
@@ -1,23 +1,46 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using System;
 
 namespace BackGroundWorker
 {
     class Program
     {
+
+        public static object AuthImplicit(string projectId)
+        {
+            // If you don't specify credentials when constructing the client, the
+            // client library will look for credentials in the environment.
+            var credential = GoogleCredential.GetApplicationDefault();
+            var storage = StorageClient.Create(credential);
+            // Make an authenticated API request.
+            var buckets = storage.ListBuckets(projectId);
+            foreach (var bucket in buckets)
+            {
+                Console.WriteLine(bucket.Name);
+            }
+            return null;
+        }
+
+
         static void Main(string[] args)
         {
-           
 
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"F:\SCOALA\Facultate\An_4_Sem_1\DATC\LABORATOR\Datc-5dd71698add6.json");
+            string value = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+            object obj = AuthImplicit("3ab8a4923fd8129d338ac4b3b9214c96c833a4cd");
 
+            /*
+                        while (true)
+                        {
+                            // FetchRandomValues();
 
-            while(true)
-            {
-                // FetchRandomValues();
+                            // CheckHumidityTresholds();
 
-                // CheckHumidityTresholds();
+                            //RefreshHeatMap()
+                        }*/
 
-                //RefreshHeatMap()
-            }
+            Console.ReadLine();
 
         }
     }
