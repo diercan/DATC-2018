ce4de342e166a4101559e10cee19fcd6ef0e640f
DianaNitu <38443483+DianaNitu@users.noreply.github.com>
2018-11-26 16:46:54 +0200

    Add files via upload

---
+++ Proiect/SmartParking/Diana/ParkingLot2.java
@@ -0,0 +1,15 @@
+package com.example.nitu.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParkingLot2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parking_lot2);
+
+        setTitle("ParkingLot2");
+    }
+}
