91bc8b4584bf3ce01092a2a0cf93f10235ce654b
Bianca <biancaa.busuioc@yahoo.com>
2018-12-29 14:34:57 +0200

    generate random data

--- BUDESCU_BOGDAN/Proiect/syntheticDataGenerator/src/app/services/http.service.ts
+++ BUDESCU_BOGDAN/Proiect/syntheticDataGenerator/src/app/services/http.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+export interface ParkStatus {
+  ParkId:number;
+  Status:number
+}
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  public BASE_URL:string ="http://localhost:3000";
+  public BASE_URL:string ="http://192.168.1.7:8081";
   constructor(private http: HttpClient) { }
 
-  public getData() {
+  public addData(data:ParkStatus[]) {
+    let body = JSON.stringify(data);
     let options = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
     };
-   return this.http.get(this.BASE_URL +"/api/park/getParkingSpaces",  options);
+   return this.http.post(this.BASE_URL +"/api/park/addStatus", body, options);
   }
 }
